36bd9f5c263f83c2d9effe05abcb04d19b141a91
Dan Caron <dancaron@gmail.com>
2023-03-22 17:19:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ class PatientData(BaseModel):
     glucose_level: float
     insulin_sensitivity_factor: float
 
-@app.post("/calculate_basal_insulin_dose/")
+@app.post("/calculate")
 async def calculate_basal_insulin_dose(patient_data: PatientData):
     basal_insulin_dose = patient_data.weight / patient_data.insulin_sensitivity_factor
     return {"basal_insulin_dose": basal_insulin_dose}
+
+@app.get("/about")
+async def about():
+    return {"about":"This is a sample application"}
