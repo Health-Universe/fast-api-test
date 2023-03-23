f1eb9e5f33e92ede186325f0eca879892d66d567
Dan Caron <dancaron@gmail.com>
2023-03-23 10:41:30 -0400

    Add cors

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://hub.healthuniverse.com",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class PatientData(BaseModel):
     weight: float
