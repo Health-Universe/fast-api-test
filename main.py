7de632c18c144bf064dbe1de6fb5dda77066edd4
Dan Caron <dancaron@gmail.com>
2023-03-25 13:45:08 -0400

    Remove cors

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-origins = [
-    "https://hub.healthuniverse.com",
-    "http://localhost",
-    "http://localhost:8000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 class DataModel(BaseModel):
     weight: float
